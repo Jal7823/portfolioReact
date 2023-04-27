18586c2ab6e04653582d6540873b2796cf0c5f57
jal7823 <jal7823@gmail.com>
2023-04-27 15:30:53 -0300

    feat: :zap: Start project
    Start project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+function Navbar() {
+  return (
+    <div className="">
+        <div className="flex justify-between mr-4 mt-4 align-middle ">
+            <img className="ml-3" style={{width:'50px',heigth:'auto'}} src="https://jalberth.pythonanywhere.com/static/img/logo.png" alt="" />
+
+            <div className="">
+                <ul className="flex text-white">
+                    <li className="ml-3"><a href="">enlace</a></li>
+                    <li className="ml-3"><a href="">enlace</a></li>
+                    <li className="ml-3"><a href="">enlace</a></li>
+                    <li className="ml-3"><a href="">enlace</a></li>
+                    <li className="ml-3"><a href="">enlace</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
